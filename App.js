71fca45d9ec4820a7602adbed447e78aa7ffcede
putueddy <bli.ipei@gmail.com>
2023-03-04 23:15:28 +0700

    Understand and Use Interpolation in JSX

--- App.js
+++ App.js
@@ -7,15 +7,33 @@ function LikeButton() {
     return "You liked this!";
   }
 
-  return React.createElement(
-    "button",
-    {
-      onClick: () => setLiked(true),
-    },
-    "Like"
+  return <button onClick={() => setLiked(true)}>Like</button>;
+}
+
+/* ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement(LikeButton)
+); */
+
+const Message = (props) => <div className="message">{props.msg}</div>;
+
+function CharacterCount({ text }) {
+  return (
+    <div>
+      {`The text "${text}" has `}
+      {text.length ? <strong>{text.length}</strong> : "No"}
+      {" characters"}
+    </div>
   );
 }
 
-const rootNode = document.getElementById("root");
-const root = ReactDOM.createRoot(rootNode);
-root.render(React.createElement(LikeButton));
+const element = (
+  <div className="container">
+    <Message msg="Hello World" />
+    <Message msg="Goodbye World" />
+    <CharacterCount text="Hello World" />
+    <CharacterCount text="" />
+    <LikeButton />
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(element);
