4260b8d7e1f5b4ab993c232929c2490c0333a5df
putueddy <bli.ipei@gmail.com>
2023-03-05 14:16:24 +0700

    Render Step

--- App.js
+++ App.js
@@ -1,39 +1,73 @@
 "use strict";
 
-function LikeButton() {
-  const [liked, setLiked] = React.useState(false);
+function RenderStep(props) {
+  const renderStep = [1, 2, 3, 4];
 
-  if (liked) {
-    return "You liked this!";
-  }
-
-  return <button onClick={() => setLiked(true)}>Like</button>;
-}
-
-/* ReactDOM.createRoot(document.getElementById("root")).render(
-  React.createElement(LikeButton)
-); */
-
-const Message = (props) => <div className="message">{props.msg}</div>;
-
-function CharacterCount({ text }) {
   return (
-    <div>
-      {`The text "${text}" has `}
-      {text.length ? <strong>{text.length}</strong> : "No"}
-      {" characters"}
+    <div className="render--step">
+      {renderStep.map((i, index) => {
+        return (
+          <div className="step" key={index}>
+            <div
+              className="step--circle"
+              style={
+                i <= props.step
+                  ? {
+                      backgroundColor: "var(--primary-color)",
+                      color: "var(--neutral-100)",
+                    }
+                  : {
+                      backgroundColor: "var(--neutral-300)",
+                      color: "var(--neutral-600)",
+                    }
+              }
+            >
+              {i.toString()}
+            </div>
+            {i < 4 && (
+              <div className="line--container">
+                <div className="inner">
+                  {i === props.step && (
+                    <div
+                      className="child"
+                      style={
+                        i <= props.step ? { width: "50%" } : { width: "0%" }
+                      }
+                    ></div>
+                  )}
+                  {i < props.step && (
+                    <div className="child" style={{ width: "100%" }}></div>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-const element = (
+function Form() {
+  const [step, setStep] = React.useState(1);
+
+  return (
+    <div className="form">
+      <RenderStep step={step} />
+    </div>
+  );
+}
+
+const container = (
   <div className="container">
-    <Message msg="Hello World" />
-    <Message msg="Goodbye World" />
-    <CharacterCount text="Hello World" />
-    <CharacterCount text="" />
-    <LikeButton />
+    <div className="headings">Get a project quote</div>
+    <div className="paragraphs">
+      Please fill the form below to receive a quote for your project. Feel free
+      to add as much detail as needed.
+    </div>
+
+    <Form />
   </div>
 );
 
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+ReactDOM.createRoot(document.getElementById("root")).render(container);
